13446bf830ae727d3109661006729b841aeb230d
Destro <japmc11@gmail.com>
2017-04-08 11:05:37 +0545

    Logout button added

--- index.php
+++ index.php
@@ -1,22 +1,51 @@
 <!DOCTYPE html>
 <?php
-  $_SESSION['message']="";
-?>
+  session_start();
+  $_SESSION['message']='';
+  $conn = new mysqli("localhost","root","Destro","blood");
+  if(isset($_POST['register'])){
+header("location:register.php");
+}
+
+if($_SERVER['REQUEST_METHOD']=='POST'){  //  session_start();
+
+    $username=$conn->real_escape_string($_POST['username']);
+
+    $password=$conn->real_escape_string($_POST['password']);
+    $password=md5($password);
+
+    $get="SELECT * FROM users WHERE username='$username' AND password='$password'";
+
+       $result=$conn->query($get);
+       if(mysqli_num_rows($result)== 1){
+         $_SESSION['message']="You are now logged in !";
+         $_SESSION['username']=$username;
+         header("location:home.php");
+       }
+       else{
+         $_SESSION['message']="username or password did not matched!";
+
+       }
+
+    }
+  ?>
+
 <html>
   <head>
     <meta charset="utf-8">
     <title>Blood Quest</title>
     <link rel="stylesheet" href="css/form.css">
+     <link rel="shortcut icon" href="img/blood.png">
   </head>
   <body>
     <center>
     <h1>Welcome to Blood Quest</h1>
     <div class="firstpageheader">
-      <img src="img/dev.png" alt="developer image">
+      <img src="img/blood-quest1.png" alt="logo">
     </div>
     <h3>Login:</h3>
     <div class="form">
-    <form class="form" action="home.php" method="post" enctype="multipart/form-data" autocomplete="off">
+    <form class="form" action="index.php" method="post" enctype="multipart/form-data" autocomplete="off">
       <div class="alert alert-error"><?=$_SESSION['message'] ?></div>
       <table>
         <tr>
@@ -33,15 +62,16 @@
       </table>
     </form>
   </div><br>
+  <form action="index.php" method="POST">
   <table>
     <tr>
       <td>Not registerd yet?</td>
     </tr>
     <tr>
-      <td>  <a href="register.php"><input type="submit" value="Register Now" name="register" class="btn btn-block btn-primary"></a></td>
+      <td>  <input type="submit" value="Register Now" name="register" class="btn btn-block btn-primary"></td>
     </tr>
   </table>
-  </table>
+</form>
 </center>
   </body>
 </html>
