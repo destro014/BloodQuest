2bab1f956ae99ca65edfa9b7547b89acf1b5d769
Destro <destro014@users.noreply.github.com>
2017-04-08 11:09:57 +0545

    Slight change in code.

--- register.php
+++ register.php
@@ -2,14 +2,10 @@
 <?php
   session_start();
   $_SESSION['message']='';
-  //create connection
-  $conn = new mysqli('localhost','root','greenland','blood');
-  //check connection
-  if($conn->connect_error){
-  die("Connection Failed:".$conn->connect_error);
-}
+  $conn = new mysqli("localhost","root","Destro","blood");
 
-if($_SERVER['REQUEST_METHOD']=='POST'){  
+
+if($_SERVER['REQUEST_METHOD']=='POST'){  //  session_start();
     $firstname=$conn->real_escape_string($_POST['firstname']);
     $lastname=$conn->real_escape_string($_POST['lastname']);
     $username=$conn->real_escape_string($_POST['username']);
@@ -39,15 +35,8 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
         $_SESION['message']="Only upload image of .JIF, .JPG, .BMP images";
       }
 */
-      $sql = "INSERT INTO users (firstname,lastname,username,email,password,bloodgroup,location,phone,gender) VALUES('$firstname','$lastname','$username','$email','$password','$bloodgroup','$location','$phone','$gender')";
-       /*if($conn->query($sql)===true){
-        echo "New Record created successfully";
-       }
-
-       else{
-        echo"Echo:".$sql."<br>".$conn->error;
-       }*/
-       $conn->query($sql);
+      $insert= "INSERT INTO users (firstname,lastname,username,email,password,bloodgroup,location,phone,gender) VALUES('$firstname','$lastname','$username','$email','$password','$bloodgroup','$location','$phone','$gender')";
+       $conn->query($insert);
        $_SESSION['message']="You are now logged in !";
        $_SESSION['username']=$username;
        header("location:home.php");
@@ -68,12 +57,13 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     <meta charset="utf-8">
     <title>Register</title>
     <link rel="stylesheet" href="css/form.css">
+    <link rel="shortcut icon" href="img/blood.png">
   </head>
   <body>
     <div class="body-content">
   <div class="module">
     <h1>Create an account</h1>
-    <form class="form" action="<?php echo($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data" autocomplete="on">
+    <form class="form" action="register.php" method="post" enctype="multipart/form-data" autocomplete="on">
       <div class="alert alert-error"><?= $_SESSION['message']?></div>
       <table>
         <tr>
@@ -95,33 +85,51 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
         <td><input type="password" placeholder="Confirm Password" name="confirmpassword" autocomplete="new-password" required /></td>
         </tr>
         <tr>
-          <td><input type="text" placeholder="Blood Group in capital letter" name="bloodgroup" required /></td>
+          <td>
+            <select  name="bloodgroup" required>
+              <option selected disabled="">Select Blood Group</option>
+              <option value="A+">A+</option>
+              <option value="A-">A-</option>
+              <option value="B+">B+</option>
+              <option value="B-">B-</option>
+              <option value="AB+">AB+</option>
+              <option value="AB-">AB-</option>
+              <option value="O+">O+</option>
+              <option value="O-">O-</option>
+            </select>
+          </td>
         </tr>
         <tr>
-          <td><input type="text" placeholder="Location" name="location" required /></td>
+          <td>
+            <select  name="location" required>
+              <option selected disabled >Select Your Location</option>
+              <option value="Kathmandu">Kathmandu</option>
+              <option value="Bhaktapur">Bhaktapur</option>
+              <option value="Lalitpur">Lalitpur</option>
+            </select>
+        </td>
         </tr>
         <tr>
           <td><input type="number" placeholder="Phone Number:" name="phone" required /></td>
         </tr>
-         <tr> 
-           <td>Gender:<input type="radio" placeholder="Gender" name="gender" value="Female" required>Female
-                <input type="radio"  placeholder="Gender" name="gender" value="Male" required>Male<br><br>
-            </td>
-         </tr>   
-
+        <tr>
+          <td>
+            Gender:&nbsp;<input type="radio" placeholder="Gender" name="gender" value="Female" required> Female&nbsp;
+                  <input type="radio"  placeholder="Gender" name="gender" value="Male" required> Male
+          </td>
+        </tr>
       <tr>
           <td>
              <div class="avatar"><label>Select your avatar: </label><input type="file" name="avatar" accept="image/*" required /></div>
           </td>
         </tr>
-
         <tr>
           <td><input type="submit" value="Register" name="register" class="btn btn-block btn-primary" /></td>
         </tr>
       </table>
-     
     </form>
   </div>
 </div>
+
   </body>
 </html>
